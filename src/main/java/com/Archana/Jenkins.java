0f363e35a7bc8a11c623b21095a7473ca2b5b5f2
Archana1611 <91115128+Archana1611@users.noreply.github.com>
2021-10-02 21:11:42 +0530

    Add files via upload

---
+++ src/main/java/com/Archana/Jenkins.java
@@ -0,0 +1,11 @@
+package com.Archana;
+
+public class Jenkins{
+	
+	public int addNum(int a, int b) {
+		return a+b;
+	}
+	public int SubNum(int a, int b) {
+		return a-b;
+	}
+}
---
+++ src/test/java/com/Archana/JenkinsTest.java
@@ -0,0 +1,21 @@
+package com.Archana;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JenkinsTest {
+
+	@Test
+	public void Addtest() {
+		Jenkins mycal = new Jenkins();
+		assertEquals(10,mycal.addNum(5,5));
+	}
+	
+	@Test
+	public void Subtest() {
+		Jenkins mycal = new Jenkins();
+		assertEquals(5,mycal.SubNum(10,5));
+	}
+
+}
